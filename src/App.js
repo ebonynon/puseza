41c20526a5858ab92733b665db62660f8dfa3cc4
ebonynon <sithumsandeepap@gmail.com>
2020-07-11 15:11:26 +0530

    done:0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
 import axios from "axios";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-//#import TextField from "@material-ui/core/TextField";
-//import Button from "@material-ui/core/Button";
-//import IconButton from "@material-ui/core/IconButton";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -13,12 +9,6 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
 import Grid from "@material-ui/core/Grid";
-//import Lightbox from "react-image-lightbox";
-// @material-ui/icons
-//import ShareIcon from "@material-ui/icons/Share";
-// core components
-
-//import "react-image-lightbox/style.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,39 +16,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function App(props) {
+function MainPage(props) {
   const classes = useStyles();
-  const [ResData, setResData] = useState([]);
-  const [Image, setImage] = React.useState({ isOpen: false });
-  const { isOpen } = Image;
-
-  useEffect(() => {
-    function fetchPart(props) {
-      axios
-        .get("https://mern-01.now.sh/api/books")
-        .then((res) => {
-          console.log("Print-ShowPartSection-API-response: " + res.data);
-          setResData(res.data);
-        })
-        .catch((err) => {
-          console.log("Error from ShowPartSection");
-        });
-    }
-    fetchPart(props);
-  }, []);
+  const part = props.part;
 
   function createData(prop, val) {
     return { prop, val };
   }
 
   const rows = [
-    createData("title", `${ResData.title}`),
-    createData("isbn", `${ResData.isbn}`),
-    createData("author", `${ResData.author}`),
-    createData("description", `${ResData.description}`),
-    createData("published_date", `${ResData.published_date}`),
-    createData("publisher", `${ResData.publisher}`),
-    createData("updated_date", `${ResData.updated_date}`),
+    createData("title", `${part.title}`),
+    createData("isbn", `${part.isbn}`),
+    createData("author", `${part.author}`),
+    createData("description", `${part.description}`),
+    createData("published_date", `${part.published_date}`),
+    createData("publisher", `${part.publisher}`),
+    createData("updated_date", `${part.updated_date}`),
   ];
 
   return (
@@ -87,4 +60,37 @@ export default function App(props) {
       </Grid>
     </div>
   );
+}
+
+export default function App(props) {
+  const [ResData, setResData] = useState([]);
+
+  useEffect(() => {
+    function fetchPart() {
+      axios
+        .get("https://mern-01.now.sh/api/books")
+        .then((res) => {
+          console.log("Print-ShowPartSection-API-response: " + res.data);
+          setResData(res.data);
+        })
+        .catch((err) => {
+          console.log("Error from ShowPartSection");
+        });
+    }
+    fetchPart();
+  }, []);
+
+  console.log("XX :: ", ResData);
+
+  var CardSectionList;
+
+  if (!ResData) {
+    CardSectionList = "Part is not availabe";
+  } else {
+    CardSectionList = ResData.map((book) => (
+      <MainPage part={book} key={book._id} />
+    ));
+  }
+
+  return (<div> {CardSectionList} </div>);
 }
